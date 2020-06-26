3c3853294d5eebbcceb9e0c503bcff8b627857fe
Spencer Kinsey-Korzym <kinseykorzysh@vcu.edu>
2020-06-26 02:30:51 -0400

    Changed firebase auth logic. Changed sign up button from a hreff to button

--- Code/scripts/index.js
+++ Code/scripts/index.js
@@ -1,16 +1,25 @@
 var nameField = document.getElementById("name");
 var email;
-var pass;
+var password;
 var confirmPass = document.getElementById("confirm-pass");
 var submitBtn = document.getElementById("submit");
 var state = false;
 const database = firebase.database();
 const auth = firebase.auth();
 
-submitBtn.addEventListener("click", (e) => {
+function signup(obj) {
   const gender = document.querySelectorAll('input[name="gender"]');
   this.email = document.getElementById("email").value;
-  this.pass = document.getElementById("signup-pwd").value; 
+  this.password = document.getElementById("signup-pwd").value;
+  if (email.length < 4) {
+    alert("Please enter an email address.");
+    return;
+  }
+  if (password.length < 4) {
+    alert("Please enter a password.");
+    return;
+  }
+
   var selectedVal;
   for (const radioBtn of gender) {
     if (radioBtn.checked) {
@@ -18,24 +27,43 @@ submitBtn.addEventListener("click", (e) => {
     }
     //TODO handler for if no box is checked
   }
-  auth.createUserWithEmailAndPassword(email, pass);
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == "auth/weak-password") {
+        alert("The password is too weak.");
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+  // [END createwithemail]
   database.ref("Users").set({
     Name: nameField.value,
     Email: email,
     Password: pass,
     Gender: selectedVal,
   });
-});
+  $(obj).css("display", "flex");
+}
+
 function nav() {
   var menu = document.querySelector("ul");
   menu.classList.toggle("active");
 }
 function passCount(obj) {
-  document.getElementById("pass").innerHTML = obj.value.length + "/10";
-  console.log(obj.value.length);
+  
+  document.getElementById("pass").innerHTML = obj.value.length + "/6";
+ 
 }
 function confirmCount(obj) {
-  document.getElementById("confirm-pass").innerHTML = obj.value.length + "/10";
+  document.getElementById("confirm-pass").innerHTML = obj.value.length + "/6";
 }
 
 function openPopup(obj) {
@@ -50,13 +78,13 @@ function toggle() {
     document.getElementById("login-pwd").type = "password";
     document.getElementById("signup-pwd").type = "password";
     document.getElementById("signup-cpwd").type = "password";
-    $('.fas').removeClass("fa-eye").addClass("fa-eye-slash");
+    $(".fas").removeClass("fa-eye").addClass("fa-eye-slash");
     state = false;
   } else {
     document.getElementById("login-pwd").type = "text";
     document.getElementById("signup-pwd").type = "text";
     document.getElementById("signup-cpwd").type = "text";
-    $('.fas').removeClass("fa-eye-slash").addClass("fa-eye");
+    $(".fas").removeClass("fa-eye-slash").addClass("fa-eye");
     state = true;
   }
 }
